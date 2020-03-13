c135edffb5b51ff20069f173044fc95c3ce6ac90
Ravi Singh <ravi.singh@adapty.com>
2020-03-13 19:37:52 +0530

    initial commit of the project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+  <modal/>
+</div>
+</template>
+
+<script>
+import modal from './components/modal';
+
+export default {
+  name: 'App',
+
+  components: {
+    modal,
+  }
+
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/modal.vue
@@ -0,0 +1,202 @@
+<template>
+<div>
+<div class="modal" :class ="{'is-active' : dialog}">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <!-- Any other Bulma elements you want -->
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
+    <div id = "dropdownMenu" class="dropdown is-active is-up">
+      <div class="dropdown-trigger">
+        <button class="button" @click = "openDropDown" aria-haspopup="true" aria-controls="dropdown-menu">
+          <span>Click ME!</span>
+          <span class="icon is-small">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </div>
+      <div class="dropdown-menu" id="dropdown-menu" role="menu">
+        <div class="dropdown-content">
+          <a href="#" class="dropdown-item">
+            Node
+          </a>
+          <a class="dropdown-item">
+            Scala
+          </a>
+          <a href="#" class="dropdown-item is-active">
+            Golang
+          </a>
+          <a href="#" class="dropdown-item">
+            Ruby-On-Rails
+          </a>
+          <a href="#" class="dropdown-item">
+            Closure
+          </a>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  <button class="modal-close is-large" @click = "dialog = false" aria-label="close"></button>
+</div>
+<button class = "button is-info" @click = "openModal">Open Modal</button>
+</div>
+</template>
+
+<script>
+  export default {
+  data(){
+    return{
+      dropdown : false,
+      dialog : false,
+      tech : ["Javscript","Nest","Golang","Scala","Closure","Ruby-On-Rails"]
+    }
+  },
+  methods : {
+    openDropDown(){
+      const dropdown = document.querySelector('.dropdown');
+      dropdown.classList.toggle('is-active');
+     },
+     openModal(){
+       this.dialog = true;
+       const dropdown = document.querySelector('.dropdown');
+       const dropdownMenu = document.getElementById('dropdownMenu');
+       if(dropdown.classList.contains("dropdown")){
+        dropdownMenu.classList.remove('is-active');
+      }
+     },
+     dropDownItem(){
+       console.log('this and e>>>>>>>>>>',this,event)
+     }
+    }
+  }
+</script>
+
+<style lang="sass" src="bulma"></style>
+<style>
+    .dropdown{
+      margin: 0 auto;
+      z-index: 999;
+      display: table;
+    }
+</style>
