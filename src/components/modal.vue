56096500d2799e68a7bc27a9eb98adbaebdfc047
Ravi Singh <ravi.singh@adapty.com>
2020-03-13 23:58:53 +0530

    close modal issue reverted

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -4,7 +4,7 @@
   <div class="modal-background"></div>
   <div class="modal-content">
     <!-- Any other Bulma elements you want -->
-    <div class="card" @click = "forceClsDp">
+    <div class="card">
       <div class="card-image">
         <figure class="image is-4by3">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
