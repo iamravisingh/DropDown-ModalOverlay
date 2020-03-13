bb1cbc7a17e381f132d104e107edefa50c2ccf44
Ravi Singh <ravi.singh@adapty.com>
2020-03-13 22:38:27 +0530

    is-up class removed

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -4,7 +4,7 @@
   <div class="modal-background"></div>
   <div class="modal-content">
     <!-- Any other Bulma elements you want -->
-    <div class="card">
+    <div class="card" @click = "forceClsDp">
       <div class="card-image">
         <figure class="image is-4by3">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
@@ -126,7 +126,7 @@
           <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
       </div>
-    <div id = "dropdownMenu" class="dropdown is-active is-up">
+    <div id = "dropdownMenu" class="dropdown is-active">
       <div class="dropdown-trigger">
         <button class="button" @click = "openDropDown" aria-haspopup="true" aria-controls="dropdown-menu">
           <span>Click ME!</span>
@@ -184,9 +184,6 @@
        if(dropdown.classList.contains("dropdown")){
         dropdownMenu.classList.remove('is-active');
       }
-     },
-     dropDownItem(){
-       console.log('this and e>>>>>>>>>>',this,event)
      }
     }
   }
